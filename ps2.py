3dc720119383e2e55ec15a935ee89c937335cb47
Amit Bikram Mishra <bikramamitabm@gmail.com>
2025-02-27 11:55:47 +0530

    Python Basics Done

---
+++ ps2.py
@@ -0,0 +1,59 @@
+#1
+#num = int(input("Enter a number: "))
+#if num > 0:
+#    print("Positive")
+#else:
+#    print("Not Positive")
+
+#2
+#num = int(input("Enter a number: "))
+#if num%2 == 0:
+#    print("even")
+#else:
+#    print("odd")
+
+#3
+#num1 = int(input("Enter a number1: "))
+#num2 = int(input("Enter a number2: "))
+#op = input("Enter operator: ")
+
+#if op == "+":
+#    print("Sum is ", num1+num2)
+#elif op == "-":
+#    print("Difference is ", num1-num2)
+#elif op == "*":
+#    print("Product is ", num1*num2)
+#elif op == "/":
+#    print("Quotient ", num1/num2)
+#elif op == "%":
+#    print("Remainder is ", num1%num2)
+#elif op == "//":
+#    print("Floor division quotient is ", num1//num2)
+#else:
+#    print("Invalid operator!!!")
+
+
+#4
+#l = input("Enter an alphabet: ")
+#if l=="a" or l=="e" or l=="i" or l=="o" or l=="u":
+#    print("vowels")
+#elif l=="A" or l=="E" or l=="I" or l=="O" or l=="U":
+#    print("VOWELS")
+#else:
+#    print("Not vowels")
+
+
+#5
+#num = int(input("Enter number (less than 5 digits): "))
+#if num >= 0 and num <= 9:
+#    print("1 digit")
+#elif num >= 10 and num <= 99:
+#   print("2 digit")
+#lif num >= 100 and num <= 999:
+#    print("3 digit")
+#elif num >= 1000 and num <= 9999:
+#    print("4 digit")
+#elif num >= 10000 and num <= 99999:
+#    print("5 digit")
+#else:
+#    print("greater than 5 digit")   

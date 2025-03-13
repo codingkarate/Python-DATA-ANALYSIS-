a9c08114570bc618c3bc7b943637bee22da62a3a
Amit Bikram Mishra <bikramamitabm@gmail.com>
2025-03-14 00:47:02 +0530

    Pattern & Strings Practice Done

---
+++ ps4.py
@@ -0,0 +1,13 @@
+A = "Why fit in, When you are Born to Stand Out!"
+
+print(len(A),"\n")
+
+print(A.count("o"), "\n")
+
+print(A.lower(), "\n")
+
+print(A.upper(), "\n")
+
+print(A.title(), "\n")
+
+print(A.find("fit in"))
---
+++ ps5(pattern).py
@@ -0,0 +1,66 @@
+for i in range (1,6):
+    for j in range (1,i+1):
+        print(j, end = " ")
+    print()
+print("\n")
+
+for i in range (1,6):
+    for j in range (1,i+1):
+        print("*", end = " ")
+    print()
+print("\n")
+
+for i in range (1,6):
+    for j in range (1,i+1):
+        print(i, end = " ")
+    print()
+print("\n")
+
+for i in range (1,6):
+    for j in range (6,i,-1):
+        print(j, end = " ")
+    print()
+print("\n")
+
+
+for i in range (1,6):
+    for j in range (6,i,-1):
+        print(i, end = " ")
+    print()
+print("\n")
+
+
+for i in range (1,6):
+    for j in range (6,i,-1):
+        print("*", end = " ")
+    print()
+print("\n")
+
+
+for i in range (1,6):
+    for j in range (5,i, -1):
+        print(" ", end = " ")
+    for k in range (i):
+        print("*", end = " ")
+    print()
+print("\n")
+
+for i in range(1,6):
+    for j in range(i, 0, -1):
+        print(j, end = " ")
+    print()
+print("\n")
+
+
+for i in range (1,6):
+    for j in range (1,i+1):
+        print("*", end = " ")
+    print()
+for i in range (1,6):
+    for j in range (5,i,-1):
+        print("*", end = " ")
+    print()
+print("\n")
+
+
+
